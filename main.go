249abdecc51299fecb5ae9d905a3841ffc32e774
Ryo Ota <nwtgck@gmail.com>
2019-01-31 02:32:46 +0900

    [add] Add "-n" option to specify the number of byte replication

--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"github.com/spf13/cobra"
 	"os"
 )
 
-func main () {
-	// TODO: hard code
-	ampN := 10
+var ampN int
+
+func init() {
+	cobra.OnInitialize()
+	RootCmd.Flags().IntVarP(&ampN,  "n", "n", 10, "The number of byte replication")
+}
+
+var RootCmd = &cobra.Command{
+	Use:   os.Args[0],
+	Short: "verbose",
+	Long:  "Verbose input",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return verbose(ampN)
+	},
+}
+
+func verbose(ampN int) error {
 	// One byte slice
 	bs := make([]byte, 1)
 	for {
@@ -18,9 +34,17 @@ func main () {
 			break
 		}
 		if err != nil {
-			panic(err)
+			return err
 		}
 		// Write amplified bytes
 		os.Stdout.Write(bytes.Repeat(bs, ampN))
+	}
+	return nil
+}
+
+func main () {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
 	}
 }
