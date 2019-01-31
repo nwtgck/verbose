64ecacd8ede6e2cca9234194e7c8796ebfee3c89
Ryo Ota <nwtgck@gmail.com>
2019-01-31 13:13:02 +0900

    [refactor] Use bufio.Reader in Decode() for better performance

--- verbose.go
+++ verbose.go
@@ -1,6 +1,7 @@
 package verbose
 
 import (
+	"bufio"
 	"io"
 )
 
@@ -28,21 +29,21 @@ func Encode(ampN int, r io.Reader, w io.Writer) error {
 func Decode(ampN int, r io.Reader, w io.Writer) error {
 	// One byte slice
 	bs := make([]byte, 1)
+	br := bufio.NewReader(r)
 	for {
-		// Read byteS
-		// TODO: Use more efficient way to skip
-		for i := 0; i < ampN; i++ {
-			n, err := r.Read(bs)
-			// Quit if EOF
-			if n == 0 {
-				return nil
-			}
-			if err != nil {
-				return err
-			}
+		// Read one byte
+		n, err := br.Read(bs)
+		// Quit if EOF
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			return err
 		}
 		// Write top byte
 		w.Write([]byte{bs[0]})
+		// Skip bytes
+		br.Discard(ampN - 1)
 	}
 	return nil
 }
